b30af9c9e0f2654d86bd43b61e0a7fa75ee1f2bd
음정태 <dmawjdxo12@naver.com>
2024-04-14 15:28:47 +0900

    리액트 쿼리 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import Home from "./pages/Home";
-import { RouterProvider ,createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,6 +18,8 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider  router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
 import usePageStore from "../store/useStore";
+import { useFetchData } from './../api/apiUtils';
 
 export default function Home() {
   const { currentPage } = usePageStore();
+  const pokemonIds = [172, 4, 147];
+
+  const { data, isLoading, error } = useFetchData(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonIds[0]}/`
+  );
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: </div>;
+  if (!data || data.length === 0) return <div>No data available</div>;
+
+  console.log(data);
   return <>{currentPage}</>;
 }
